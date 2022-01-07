77583c935100632814062b60827d886a80673da9
kaneflida <junaidwaghu@gmail.com>
2022-01-07 22:50:34 +0530

    Installed font bitter and montserrat and resetted default browser styles using normalize.css ,added react-router-dom package and added routes to the project at / and /search, folder structure set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import styles from './App.module.css';
+import MarketingPage from './Pages/MarketingPage';
 
 function App() {
   return (
-    <div>
+    <div className={styles.AppContainer}>
       App Placeholder
+      <Routes>
+        <Route path="/" element={<MarketingPage />} />
+        <Route path="/search" element={<MarketingPage />} />
+      </Routes>
     </div>
   );
 }
